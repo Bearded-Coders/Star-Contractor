9c7b901b89d3079c82b9e4fd73b8dc0fce059e95
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-03 12:50:41 -0400

    initial commit and ignore

---
+++ src/main/java/com/example/star_contractor/StarContractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.star_contractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarContractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarContractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/star_contractor/StarContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.star_contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarContractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
