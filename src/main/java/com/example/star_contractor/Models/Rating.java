de1174970dc1a0cbca04cc756bf8a121529368af
SSPENELOPE <typoepping@gmail.com>
2023-07-10 14:11:50 -0400

    added getters/setters and constructor to Rating model

--- src/main/java/com/example/star_contractor/Models/Rating.java
+++ src/main/java/com/example/star_contractor/Models/Rating.java
@@ -18,5 +18,57 @@ public class Rating implements Serializable {
     @ManyToOne
     private User ratedUserId;
 
-   //delete this later
+    public Rating() {
+    }
+    public Rating(Integer id, Short rating, User ratedUserId) {
+        this.id = id;
+        this.rating = rating;
+        this.ratedUserId = ratedUserId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Short getRating() {
+        return rating;
+    }
+
+    public void setRating(Short rating) {
+        this.rating = rating;
+    }
+
+    public User getRatedUserId() {
+        return ratedUserId;
+    }
+
+    public void setRatedUserId(User ratedUserId) {
+        this.ratedUserId = ratedUserId;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "id=" + id +
+                ", rating=" + rating +
+                ", ratedUserId=" + ratedUserId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rating rating1 = (Rating) o;
+        return Objects.equals(id, rating1.id) && Objects.equals(rating, rating1.rating) && Objects.equals(ratedUserId, rating1.ratedUserId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, rating, ratedUserId);
+    }
 }
