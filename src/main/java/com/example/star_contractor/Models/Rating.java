c327670008eec34d8304043c47af05c69d5703db
SSPENELOPE <typoepping@gmail.com>
2023-07-10 14:01:06 -0400

    Added rating model and relations

---
+++ src/main/java/com/example/star_contractor/Models/Rating.java
@@ -0,0 +1,20 @@
+package com.example.star_contractor.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+@Entity
+@JsonIgnoreProperties
+public class Rating implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Short rating;
+
+    @ManyToOne
+    private User ratedUserId;
+}
