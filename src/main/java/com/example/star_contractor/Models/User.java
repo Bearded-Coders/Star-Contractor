e5da71bd1f53b32678507290a4c2a6a86096daba
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-10 12:10:49 -0400

    added user model

---
+++ src/main/java/com/example/star_contractor/Models/User.java
@@ -0,0 +1,35 @@
+package com.example.star_contractor.Models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String startingArea;
+    @Column()
+    private String profilePic;
+    @Column()
+    private int avgRating;
+
+    @ManyToOne(mappedBy = "creator_id")
+    private List<jobs> myJobs;
+
+
+
+
+
+
+}
