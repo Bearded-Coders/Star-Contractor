43a637a0d9843f681a745ec9aec90b77fb88b2bd
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-11 19:14:10 -0400

    added linkable pages

--- src/main/java/com/example/star_contractor/Controllers/JobController.java
+++ src/main/java/com/example/star_contractor/Controllers/JobController.java
@@ -1,7 +1,13 @@
 package com.example.star_contractor.Controllers;
 
+import com.example.star_contractor.Models.Jobs;
+import com.example.star_contractor.Repostories.JobRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class JobController {
+
 }
