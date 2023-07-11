dc021f4788438b87e75c9b614a21068838c45f47
SSPENELOPE <typoepping@gmail.com>
2023-07-11 18:43:28 -0400

    refactored JobController to work with Thymeleaf. Edited html pages to test backend which does now work

--- src/main/java/com/example/star_contractor/Controllers/JobController.java
+++ src/main/java/com/example/star_contractor/Controllers/JobController.java
@@ -2,28 +2,47 @@ package com.example.star_contractor.Controllers;
 
 import com.example.star_contractor.Models.Jobs;
 import com.example.star_contractor.Repostories.JobRepository;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class JobController {
-    @Autowired
-    JobRepository jobsRepository;
+//    @Autowired
+//    JobRepository jobsRepository;
+
+    private final JobRepository jobsRepository;
+
+    public JobController(JobRepository jobsRepository) {
+        this.jobsRepository = jobsRepository;
+    }
 
     @GetMapping("/jobs")
-    public List<Jobs> getAllJobs() {
-        return jobsRepository.findAll();
+    public String getAllJobs(Model model) {
+        List<Jobs> jobs = jobsRepository.findAll();
+
+        model.addAttribute("job", jobs);
+
+        return "index/jobposts";
+
     }
 
     @GetMapping("/jobs/{id}")
-    public Jobs getJob(@PathVariable Integer id) throws Exception {
-        Jobs job = jobsRepository.getJobById(id);
+    public String getJob(@PathVariable Integer id, Model model) throws Exception {
+        Jobs singleJob = jobsRepository.getJobById(id);
+
+        model.addAttribute("singleJob", singleJob);
+
+        return "index/jobdetails";
+    }
+
+    @PostMapping("/jobs")
+    public Jobs addJob(@RequestBody Jobs job) {
+        jobsRepository.save(job);
         return job;
     }
 }
