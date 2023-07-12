56c718156fea101a1c7e831b680cf797c716cd6d
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-12 17:03:10 -0400

    added categories display to job details.

--- src/main/java/com/example/star_contractor/Repostories/CategoriesRepository.java
+++ src/main/java/com/example/star_contractor/Repostories/CategoriesRepository.java
@@ -1,9 +1,13 @@
 package com.example.star_contractor.Repostories;
 
 import com.example.star_contractor.Models.Categories;
+import com.example.star_contractor.Models.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoriesRepository extends JpaRepository<Categories, Integer> {
+    List<Categories> findCategoriesByJobId(Jobs id);
 }
