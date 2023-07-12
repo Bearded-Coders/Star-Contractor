50a905c2c1b7f5d54c700f0521575b037e3c6670
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-12 11:55:36 -0400

    added applicants list for each job

--- src/main/java/com/example/star_contractor/Controllers/JobController.java
+++ src/main/java/com/example/star_contractor/Controllers/JobController.java
@@ -1,31 +1,34 @@
 package com.example.star_contractor.Controllers;
 
 import com.example.star_contractor.Models.Jobs;
+import com.example.star_contractor.Models.User;
 import com.example.star_contractor.Repostories.JobRepository;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 public class JobController {
 
     private final JobRepository jobsRepository;
+    private final JobRepository userDao;
 
-    public JobController(JobRepository jobsRepository) {
+    public JobController(JobRepository jobsRepository, JobRepository userDao) {
         this.jobsRepository = jobsRepository;
+        this.userDao = userDao;
     }
+
 
     @GetMapping("/jobs")
     public String getAllJobs(Model model) {
         List<Jobs> jobs = jobsRepository.findAll();
 
         model.addAttribute("job", jobs);
+
+
 
         return "index/jobposts";
 
@@ -36,7 +39,6 @@ public class JobController {
         Jobs singleJob = jobsRepository.getJobById(id);
 
         model.addAttribute("singleJob", singleJob);
-
         return "index/jobdetails";
     }
 
