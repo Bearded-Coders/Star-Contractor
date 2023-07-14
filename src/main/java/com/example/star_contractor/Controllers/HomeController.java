f40d0af67eb854c9acf20cec6eba39137eddaa3a
Austin Joiner <austin.j.joiner1@gmail.com>
2023-07-14 16:10:36 -0400

    added security working with features and authorizations

--- src/main/java/com/example/star_contractor/Controllers/HomeController.java
+++ src/main/java/com/example/star_contractor/Controllers/HomeController.java
@@ -1,14 +1,29 @@
 package com.example.star_contractor.Controllers;
 
+import com.example.star_contractor.Models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String goHome() {
+    public String goHome(Model model, Principal principal) {
 
+        User user = null;
+        if (principal != null) {
+            user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("user", user);
+        }
+
+        if (user != null) {
+            String userUrl = "/profile/" + user.getId();
+            model.addAttribute("userUrl", userUrl);
+        }
         return "index/home";
     }
 }
