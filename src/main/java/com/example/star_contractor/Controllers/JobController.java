ecf069717b384952f42d612bb1daa0ada484d321
SSPENELOPE <typoepping@gmail.com>
2023-07-21 08:39:26 -0400

    stash

--- src/main/java/com/example/star_contractor/Controllers/JobController.java
+++ src/main/java/com/example/star_contractor/Controllers/JobController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
