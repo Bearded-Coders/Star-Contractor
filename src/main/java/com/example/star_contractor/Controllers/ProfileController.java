bd1138a85e5d26e3b4794ce3af3e1281456102a6
SSPENELOPE <typoepping@gmail.com>
2023-08-15 11:29:24 -0400

    added features for accepting/denying friend frequest

--- src/main/java/com/example/star_contractor/Controllers/ProfileController.java
+++ src/main/java/com/example/star_contractor/Controllers/ProfileController.java
@@ -1,7 +1,9 @@
 package com.example.star_contractor.Controllers;
 
+import com.example.star_contractor.Models.Friends;
 import com.example.star_contractor.Models.Jobs;
 import com.example.star_contractor.Models.User;
+import com.example.star_contractor.Repostories.FriendRepository;
 import com.example.star_contractor.Repostories.JobRepository;
 import com.example.star_contractor.Repostories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,45 +23,141 @@ public class ProfileController {
 
     private final UserRepository userDao;
     private final JobRepository jobDao;
+    private final FriendRepository friendDao;
 
     @Value("${filestack.api.key}")
     private String filestackapi;
 
-    public ProfileController(UserRepository userDao, JobRepository jobDao) {
+    public ProfileController(UserRepository userDao, JobRepository jobDao, FriendRepository friendDao) {
         this.userDao = userDao;
         this.jobDao = jobDao;
+        this.friendDao = friendDao;
     }
 
-
+    // Get user profile
     @GetMapping("/profile/{id}")
     public String getProfile(@PathVariable Long id, Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        long userId = user.getId();
 
         User userProfile = userDao.findById(id).orElse(null);
         List<Jobs> userJobs = jobDao.findJobsByCreatorId(userDao.getReferenceById(id));
         List<Jobs> appliedJobs = jobDao.findJobsByApplicantListContains(userDao.getReferenceById(id));
-//        List<User> friendsList = userDao.findAllById(userDao.getReferenceById(id));
-        List<User> friendList = userDao.findUsersByFriendsListContaining(userProfile);
+        List<User> friends = user.getFriends();
+
+        List<Friends> receivedFriendRequests = user.getReceivedFriendRequests();
+        List<Friends> pendingFriendRequests = receivedFriendRequests.stream()
+                .filter(request -> !request.getAccepted())
+                .toList();
+
+
+
         model.addAttribute("userProfileLink", userProfile);
         model.addAttribute("myJobs", userJobs);
 //        model.addAttribute("grabId", userId);
         model.addAttribute("user", user);
         model.addAttribute("appliedJobs", appliedJobs);
         model.addAttribute("filestackapi", filestackapi);
-        model.addAttribute("friends", friendList);
-        System.out.println("************" + userProfile.getFriendsList() + "***********");
-
-
-//        System.out.println(userProfile.getStartingArea());
+        model.addAttribute("friends", friends);
+        model.addAttribute("pendingFriendRequests", pendingFriendRequests);
+        System.out.println("************" + userProfile.getFriends() + "***********");
 
         if(user.getId().equals(id)) {
+            // If the profile belongs to the user, we display the "profile" page
             return "index/profile";
         } else {
+            // Otherwise we show the "user-profile" page
             return "index/user-profile";
         }
     }
 
+    // Add a friend
+    @PostMapping("/profile/add/{id}")
+    public String addFriend(@PathVariable Long id, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User targetUser = userDao.findById(id).orElse(null); // Find the user to be added as a friend
+
+        if (currentUser != null && targetUser != null) {
+            Friends friendRequest = new Friends();
+            friendRequest.setSender(currentUser);
+            friendRequest.setReceiver(targetUser);
+            friendRequest.setRequested(true);
+            friendRequest.setAccepted(false);
+            friendRequest.setDenied(false);
+
+            currentUser.getSentFriendRequests().add(friendRequest);
+            targetUser.getReceivedFriendRequests().add(friendRequest);
+
+            friendDao.save(friendRequest);
+            userDao.save(currentUser);
+            userDao.save(targetUser);
+        }
+
+        // Redirect to the user's profile page
+        return "redirect:/profile/" + id;
+    }
+
+    // Remove a friend
+    @PostMapping("/profile/remove/{id}")
+    public String removeFriend(@PathVariable Long id, Model model) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User friendToRemove = userDao.findById(id).orElse(null); // Find the friend to be removed
+
+        if (currentUser != null && friendToRemove != null) {
+            // Remove the friend from each other's friend lists
+            currentUser.getFriends().remove(friendToRemove);
+            friendToRemove.getFriends().remove(currentUser);
+
+            userDao.save(currentUser);
+            userDao.save(friendToRemove);
+        }
+
+        // Redirect to the user's profile page
+        return "redirect:/profile/" + id;
+    }
+
+    // Accept Friend Request
+    @PostMapping("/profile/accept-friend-request/{requestId}")
+    public String acceptFriendRequest(@PathVariable Long requestId, Model model) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Friends friendRequest = friendDao.findById(requestId).orElse(null);
+
+        if (currentUser != null && friendRequest != null && friendRequest.getReceiver().equals(currentUser)) {
+            friendRequest.setAccepted(true);
+
+            User sender = friendRequest.getSender();
+            User receiver = friendRequest.getReceiver();
+
+            sender.getFriends().add(receiver);
+            receiver.getFriends().add(sender);
+
+            userDao.save(sender);
+            userDao.save(receiver);
+            friendDao.save(friendRequest);
+        }
+
+        // Redirect to the user's profile page
+        return "redirect:/profile/" + currentUser.getId();
+    }
+
+    // Deny Friend request
+    @PostMapping("/profile/deny-friend-request/{requestId}")
+    public String denyFriendRequest(@PathVariable Long requestId, Model model) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Friends friendRequest = friendDao.findById(requestId).orElse(null);
+
+        if (currentUser != null && friendRequest != null && friendRequest.getReceiver().equals(currentUser)) {
+            friendRequest.setDenied(true);
+
+            friendDao.save(friendRequest);
+        }
+
+        // Redirect to the user's profile page
+        return "redirect:/profile/" + currentUser.getId();
+    }
+
+    // Get edit profile page
     @GetMapping("/profile/edit/{userId}")
     public String showEditProfile(@PathVariable("userId") long userId, Model model) {
         // Fetch the user from the database based on the provided user ID
@@ -71,60 +169,7 @@ public class ProfileController {
         return "index/editprofile";
     }
 
-    @PostMapping("/profile/add/{id}")
-    public String addFriend(@PathVariable Long id, Model model) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        // Find the current user and the user to be added as a friend
-        User currentUser = userDao.findById(id).orElse(null);
-        User friendToAdd = userDao.findById(user.getId()).orElse(null); // Replace someOtherId with the friend's ID you want to add
-
-        // Check if both users exist and are valid
-        if (currentUser == null || friendToAdd == null) {
-            // Handle the case when user(s) are not found
-            return "redirect:/error"; // Redirect to an error page or handle it as per your application's requirements
-        }
-        model.addAttribute("friendToAdd", friendToAdd);
-        model.addAttribute("currentUser", currentUser);
-        // Add the users to each other's friend lists
-        currentUser.getFriendsList().add(friendToAdd);
-        friendToAdd.getFriendsList().add(currentUser);
-
-        // Save the changes to the database
-        userDao.save(currentUser);
-        userDao.save(friendToAdd);
-
-        // Redirect to the user's profile page
-
-        return "redirect:/profile/" + id;
-    }
-    @PostMapping("/profile/remove/{id}")
-    public String removeFriend(@PathVariable Long id, Model model) {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        // Find the current user and the user to be added as a friend
-        User currentUser = userDao.findById(id).orElse(null);
-        User friendToRemove = userDao.findById(user.getId()).orElse(null); // Replace someOtherId with the friend's ID you want to add
-
-        // Check if both users exist and are valid
-        if (currentUser == null || friendToRemove == null) {
-            // Handle the case when user(s) are not found
-            return "redirect:/error"; // Redirect to an error page or handle it as per your application's requirements
-        }
-        model.addAttribute("friendToAdd", friendToRemove);
-        model.addAttribute("currentUser", currentUser);
-        // Add the users to each other's friend lists
-        currentUser.getFriendsList().remove(friendToRemove);
-        friendToRemove.getFriendsList().remove(currentUser);
-
-        // Save the changes to the database
-        userDao.save(currentUser);
-        userDao.save(friendToRemove);
-
-        // Redirect to the user's profile page
-
-        return "redirect:/profile/" + id;
-    }
-
-
+    // Submit the edit profile
     @PostMapping("/profile/edits/{userId}")
     public String editProfile(@PathVariable("userId") long userId, @ModelAttribute("user") User updatedUser, Model model) {
         User user = userDao.getReferenceById(userId);
@@ -159,15 +204,14 @@ public class ProfileController {
         return "redirect:/profile/" + userId;
     }
 
+    // Upload image to user profile
     @PostMapping("/profile/upload")
     public String uploadProfile(@RequestParam(name = "stashFilestackURL") String uploadedProfilePic) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        user = userDao.getReferenceById((long) user.getId());
 
         user = userDao.getReferenceById((long) user.getId());
         user.setProfilePic(uploadedProfilePic);
         userDao.save(user);
-
 
         return "redirect:/profile/" + user.getId();
     }
@@ -178,4 +222,23 @@ public class ProfileController {
 //To DO
 //Add delete mapping for User delete
 //Add rating functionality and way(s) for users to input it
+
+
+//    User currentUser = userDao.findById(id).orElse(null);
+//    User friendToAdd = userDao.findById(user.getId()).orElse(null); // Replace someOtherId with the friend's ID you want to add
+//
+//// Check if both users exist and are valid
+//        if (currentUser == null || friendToAdd == null) {
+//                // Handle the case when user(s) are not found
+//                return "redirect:/error"; // Redirect to an error page or handle it as per your application's requirements
+//                }
+//                model.addAttribute("friendToAdd", friendToAdd);
+//                model.addAttribute("currentUser", currentUser);
+//                // Add the users to each other's friend lists
+//                currentUser.getFriendsList().add(friendToAdd);
+//                friendToAdd.getFriendsList().add(currentUser);
+//
+//                // Save the changes to the database
+//                userDao.save(currentUser);
+//                userDao.save(friendToAdd);
 
---
+++ src/main/java/com/example/star_contractor/Repostories/FriendRepository.java
@@ -0,0 +1,8 @@
+package com.example.star_contractor.Repostories;
+
+import com.example.star_contractor.Models.Friends;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friends, Long> {
+
+}
