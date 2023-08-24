cd881bfb8627f8d5a49dad2f9e77a74eaf02bb59
SSPENELOPE <typoepping@gmail.com>
2023-08-24 16:02:55 -0400

    jobservice update

--- src/main/java/com/example/star_contractor/Services/JobsService.java
+++ src/main/java/com/example/star_contractor/Services/JobsService.java
@@ -2,6 +2,8 @@ package com.example.star_contractor.Services;
 
 import com.example.star_contractor.Models.Jobs;
 import com.example.star_contractor.Models.User;
+import com.example.star_contractor.Repostories.CategoriesRepository;
+import com.example.star_contractor.Repostories.CommentRepository;
 import com.example.star_contractor.Repostories.JobRepository;
 import com.example.star_contractor.Repostories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Service;
 public class JobsService {
     private final UserRepository userDao;
     private final JobRepository jobDao;
+    private final CategoriesRepository catDao;
+    private final CommentRepository commentDao;
 
-    public  JobsService(UserRepository userDao, JobRepository jobDao) {
+    public  JobsService(UserRepository userDao,
+                        JobRepository jobDao,
+                        CommentRepository commentDao,
+                        CategoriesRepository catDao) {
         this.jobDao = jobDao;
+        this.catDao = catDao;
         this.userDao = userDao;
+        this.commentDao = commentDao;
     }
 
 
